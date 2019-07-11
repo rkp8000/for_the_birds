4fbe92de01bea0a340503beaa92ddf1fa0e8d32f
Rich Pang <rpang@uw.edu>
2019-07-11 14:18:59 -0700

    Add adaptive current-based LIFNtwk class.

--- ntwk.py
+++ ntwk.py
@@ -142,6 +142,7 @@ class BinarySTDPNtwk(object):
 
         return Generic(t=np.arange(n_t), vs=vs, spks=spks, thts=thts, d_w_s=d_w_s, f_ws=f_ws, ws=ws)
 
+
 # Current-based LIF network
 class LIFNtwkI(object):
     """Network of leaky integrate-and-fire neurons with *current-based* synapses."""
@@ -402,6 +403,133 @@ class LIFNtwkG(object):
         
         return Generic(dt=dt, t=t, gs=gs, vs=vs, spks=spks, spks_t=spks_t, spks_c=spks_c, i_ext=i_ext, ntwk=self)
 
+
+# Adaptive integrate-and-fire network (current-based)
+class ALIFNtwkI(object):
+    """Network of *adaptive* leaky integrate-and-fire neurons with *current-based* synapses."""
+    
+    def __init__(self, c_m, g_l, e_l, v_th, v_r, t_r, t_a, b, w_r, w_u, sparse=True):
+        # ntwk size
+        n = w_r.shape[0]
+        
+        # process inputs
+        if type(t_r) in [float, int]:
+            t_r = t_r * np.ones(n)
+            
+        if type(v_r) in [float, int]:
+            v_r = v_r * np.ones(n)
+            
+        self.n = n
+        self.c_m = c_m
+        self.g_l = g_l
+        self.t_m = c_m / g_l
+        self.e_l = e_l
+        self.v_th = v_th
+        self.v_r = v_r
+        self.t_r = t_r
+        self.t_a = t_a
+        self.b = b
+        
+        if sparse:  # sparsify connectivity if desired
+            self.w_r = csc_matrix(w_r)
+            self.w_u = csc_matrix(w_u) if w_u is not None else w_u
+        else:
+            self.w_r = w_r
+            self.w_u = w_u
+        
+    def run(self, dt, clamp, i_ext, spks_u=None):
+        """
+        Run simulation.
+        
+        :param dt: integration timestep (s)
+        :param clamp: dict of times to clamp certain variables (e.g. to initialize)
+        :param i_ext: external current inputs (either 1D or 2D array, length = num timesteps for smln)
+        :param spks_up: upstream inputs
+        """
+        n = self.n
+        n_t = len(i_ext)
+        c_m = self.c_m
+        g_l = self.g_l
+        e_l = self.e_l
+        v_th = self.v_th
+        v_r = self.v_r
+        t_r = self.t_r
+        t_r_int = np.round(t_r/dt).astype(int)
+        t_a = self.t_a
+        b = self.b
+        w_r = self.w_r
+        w_u = self.w_u
+        
+        if spks_u is not None:
+            assert len(i_ext) == len(spks_u)
+        
+        # make data storage arrays
+        vs = np.nan * np.zeros((n_t, n))
+        spks = np.zeros((n_t, n), dtype=bool)
+        
+        rp_ctr = np.zeros(n, dtype=int)  # refractory counter
+        a = np.zeros(n)  # adaptation variable
+        
+        # convert float times in clamp dict to time idxs
+        ## convert to list of tuples sorted by time
+        tmp_v = sorted(list(clamp.v.items()), key=lambda x: x[0])
+        tmp_spk = sorted(list(clamp.spk.items()), key=lambda x: x[0])
+        clamp = Generic(
+            v={int(round(t_/dt)): f_v for t_, f_v in tmp_v},
+            spk={int(round(t_/dt)): f_spk for t_, f_spk in tmp_spk})
+        
+        # loop over timesteps
+        for t_ctr in range(len(i_ext)):
+            
+            # update voltages
+            if t_ctr in clamp.v:  # check for clamped voltages
+                vs[t_ctr, :] = clamp.v[t_ctr]
+            else:  # update as per diff eq
+                v = vs[t_ctr-1, :]
+                
+                # get total current input
+                i_total = -g_l*(v - e_l)  # leak
+                
+                if t_ctr >= 1:  # synaptic
+                    
+                    if spks_u is not None:  # upstream
+                        i_total += w_u.dot(spks_u[t_ctr-1, :])
+                    i_total += w_r.dot(spks[t_ctr-1, :])  # recurrent
+                
+                i_total += i_ext[t_ctr]  # external
+                i_total += a  # adaptation
+                
+                # update v
+                vs[t_ctr, :] = v + (dt/c_m)*i_total
+                
+                # clamp v for cells still in refrac period
+                vs[t_ctr, rp_ctr > 0] = self.v_r[rp_ctr > 0]
+            
+            # update spks
+            if t_ctr in clamp.spk:  # check for clamped spikes
+                spks[t_ctr, :] = clamp.spk[t_ctr]
+            else:  # check for threshold crossings
+                spks[t_ctr, :] = vs[t_ctr, :] >= self.v_th
+                
+            # reset v and update refrac periods for nrns that spiked
+            vs[t_ctr, spks[t_ctr]] = self.v_r[spks[t_ctr]]
+            rp_ctr[spks[t_ctr]] = t_r_int[spks[t_ctr]] + 1
+            
+            # decrement refrac periods
+            rp_ctr[rp_ctr > 0] -= 1
+            
+            # update adaptation variable
+            a += (dt/t_a)*(-a + b*(vs[t_ctr, :] - e_l))
+        
+        t = dt*np.arange(n_t, dtype=float)
+        
+        # convert spks to spk times and cell idxs (for easy access l8r)
+        tmp = spks.nonzero()
+        spks_t = dt * tmp[0]
+        spks_c = tmp[1]
+        
+        return Generic(dt=dt, t=t, vs=vs, spks=spks, spks_t=spks_t, spks_c=spks_c, i_ext=i_ext, ntwk=self)
+    
     
 # Helper functions
 def join_w(targs, srcs, ws):
